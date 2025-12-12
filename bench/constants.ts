7c562c86bf51fed33f55e95f611cd6ff1a81ca0c
Theo Browne <me@t3.gg>
2025-12-11 16:48:16 -0800

    update models

--- bench/constants.ts
+++ bench/constants.ts
@@ -22,14 +22,10 @@ const defaultProviderOptions = {
 };
 
 export const modelsToRun: RunnableModel[] = [
+  // Open weight
   {
     name: "kimi-k2-thinking",
     llm: openrouter("moonshotai/kimi-k2-thinking", defaultProviderOptions),
-    reasoning: true,
-  },
-  {
-    name: "grok-4",
-    llm: openrouter("x-ai/grok-4", defaultProviderOptions),
     reasoning: true,
   },
   {
@@ -37,8 +33,67 @@ export const modelsToRun: RunnableModel[] = [
     llm: openrouter("moonshotai/kimi-k2", defaultProviderOptions),
   },
   {
-    name: "gemini-2.5-pro",
-    llm: openrouter("google/gemini-2.5-pro-preview", defaultProviderOptions),
+    name: "qwen-3-32b",
+    llm: openrouter("qwen/qwen3-32b", defaultProviderOptions),
+    reasoning: true,
+  },
+  {
+    name: "glm-4.5",
+    llm: openrouter("z-ai/glm-4.5", defaultProviderOptions),
+    reasoning: true,
+  },
+  {
+    name: "glm-4.5v",
+    llm: openrouter("z-ai/glm-4.5v", defaultProviderOptions),
+    reasoning: true,
+  },
+  {
+    name: "qwen3-235b-a22b-thinking",
+    llm: openrouter(
+      "qwen/qwen3-235b-a22b-thinking-2507",
+      defaultProviderOptions
+    ),
+    reasoning: true,
+  },
+  {
+    name: "deepseek-r1-0528",
+    llm: openrouter("deepseek/deepseek-r1-0528", defaultProviderOptions),
+    reasoning: true,
+  },
+  {
+    name: "gpt-oss-120b",
+    llm: openrouter("openai/gpt-oss-120b", defaultProviderOptions),
+    reasoning: true,
+  },
+  {
+    name: "gpt-oss-20b",
+    llm: openrouter("openai/gpt-oss-20b", defaultProviderOptions),
+    reasoning: true,
+  },
+  {
+    name: "deepseek-v3.1",
+    llm: openrouter("deepseek/deepseek-chat-v3.1", defaultProviderOptions),
+  },
+  {
+    name: "deepseek-v3.1-thinking",
+    llm: openrouter("deepseek/deepseek-chat-v3.1", defaultProviderOptions),
+    reasoning: true,
+  },
+  {
+    name: "deepseek-v3.2",
+    llm: openrouter("deepseek/deepseek-v3.2", defaultProviderOptions),
+    reasoning: true,
+  },
+
+  // Grok
+  {
+    name: "grok-4",
+    llm: openrouter("x-ai/grok-4", defaultProviderOptions),
+    reasoning: true,
+  },
+  {
+    name: "grok-4.1-fast",
+    llm: openrouter("x-ai/grok-4.1-fast", defaultProviderOptions),
     reasoning: true,
   },
   {
@@ -46,11 +101,23 @@ export const modelsToRun: RunnableModel[] = [
     llm: openrouter("x-ai/grok-3-mini-beta", defaultProviderOptions),
     reasoning: true,
   },
+
+  // Gemini
   {
-    name: "qwen-3-32b",
-    llm: openrouter("qwen/qwen3-32b", defaultProviderOptions),
+    name: "gemini-3-pro-preview",
+    llm: openrouter("google/gemini-3-pro-preview", defaultProviderOptions),
     reasoning: true,
   },
+  {
+    name: "gemini-2.5-pro",
+    llm: openrouter("google/gemini-2.5-pro-preview", defaultProviderOptions),
+    reasoning: true,
+  },
+  {
+    name: "gemini-2.5-flash",
+    llm: openrouter("google/gemini-2.5-flash", defaultProviderOptions),
+  },
+  // Anthropic
   {
     name: "claude-4-sonnet",
     llm: openrouter("anthropic/claude-sonnet-4", defaultProviderOptions),
@@ -67,10 +134,17 @@ export const modelsToRun: RunnableModel[] = [
     reasoning: true,
   },
   {
+    name: "claude-4.5-opus",
+    llm: openrouter("anthropic/claude-opus-4.5", defaultProviderOptions),
+    reasoning: true,
+  },
+  {
     name: "claude-4.5-sonnet",
     llm: openrouter("anthropic/claude-sonnet-4.5", defaultProviderOptions),
     reasoning: true,
   },
+
+  // OpenAI
   {
     name: "o4-mini",
     llm: openrouter("openai/o4-mini", defaultProviderOptions),
@@ -94,28 +168,6 @@ export const modelsToRun: RunnableModel[] = [
   {
     name: "gpt-4o",
     llm: openrouter("openai/gpt-4o", defaultProviderOptions),
-    reasoning: true,
-  },
-  {
-    name: "gemini-2.5-flash",
-    llm: openrouter("google/gemini-2.5-flash", defaultProviderOptions),
-  },
-  {
-    name: "glm-4.5",
-    llm: openrouter("z-ai/glm-4.5", defaultProviderOptions),
-    reasoning: true,
-  },
-  {
-    name: "glm-4.5v",
-    llm: openrouter("z-ai/glm-4.5v", defaultProviderOptions),
-    reasoning: true,
-  },
-  {
-    name: "qwen3-235b-a22b-thinking",
-    llm: openrouter(
-      "qwen/qwen3-235b-a22b-thinking-2507",
-      defaultProviderOptions
-    ),
     reasoning: true,
   },
 
@@ -210,65 +262,4 @@ export const modelsToRun: RunnableModel[] = [
     }),
     reasoning: true,
   },
-
-  {
-    name: "deepseek-v3.1",
-    llm: openrouter("deepseek/deepseek-chat-v3.1", defaultProviderOptions),
-  },
-  {
-    name: "deepseek-v3.1-thinking",
-    llm: openrouter("deepseek/deepseek-chat-v3.1", defaultProviderOptions),
-    reasoning: true,
-  },
-
-  {
-    name: "deepseek-r1-0528",
-    llm: openrouter("deepseek/deepseek-r1-0528", defaultProviderOptions),
-    reasoning: true,
-  },
-  {
-    name: "sonoma-dusk-alpha",
-    llm: openrouter("openrouter/sonoma-dusk-alpha", defaultProviderOptions),
-    reasoning: false,
-  },
-
-  {
-    name: "sonoma-sky-alpha",
-    llm: openrouter("openrouter/sonoma-sky-alpha", defaultProviderOptions),
-    reasoning: false,
-  },
-
-  // {
-  //   name: "gpt-oss-120b",
-  //   llm: openrouter("openai/gpt-oss-120b", defaultProviderOptions),
-  //   reasoning: true,
-  // },
-
-  // {
-  //   name: "gpt-oss-20b",
-  //   llm: openrouter("openai/gpt-oss-20b", defaultProviderOptions),
-  //   reasoning: true,
-  // },
-
-  // Unused at this point
-  // {
-  //   name: "gemini-2.0-flash",
-  //   llm: openrouter("google/gemini-2.0-flash-001", defaultProviderOptions),
-  // },
-  // {
-  //   name: "claude-3-5-sonnet",
-  //   llm: openrouter("anthropic/claude-3.5-sonnet", defaultProviderOptions),
-  // },
-  // {
-  //   name: "claude-3-7-sonnet",
-  //   llm: openrouter("anthropic/claude-3.7-sonnet", defaultProviderOptions),
-  // },
-  // {
-  //   name: "claude-3-7-sonnet-thinking",
-  //   llm: openrouter(
-  //     "anthropic/claude-3.7-sonnet:thinking",
-  //     defaultProviderOptions
-  //   ),
-  //   reasoning: true,
-  // },
 ];
